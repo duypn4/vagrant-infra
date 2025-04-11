7e7f383480727c70be74249243e5edd8ef1db434
jimmy <jimmy@mail.com>
2025-04-11 23:24:05 +0700

    finish install nodes

--- k8s/scripts/install-node.sh
+++ k8s/scripts/install-node.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-apt-get install -y apt-transport-https ca-certificates curl gpg
+# install kubeadm
+apt-get install -y apt-transport-https ca-certificates curl gpg wget
 mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update -y
-apt-get install -y --force-yes kubelet kubeadm kubectl
+apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
+
+# setup container runtime
+apt-get install -y containerd
+mkdir -p /etc/containerd
+containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' | sudo tee /etc/containerd/config.toml
+systemctl restart containerd
